e18963e15c8352c7f7e938bb37f450692e5321aa
Ahmet Serhat Kirici <ahmetserhat.kirici@gmail.com>
2025-05-20 10:35:37 +0300

    Audio updated to small model

--- helpers/audio_utils.py
+++ helpers/audio_utils.py
@@ -1,6 +1,6 @@
 import whisper
 
 def transcribe_audio(audio_path: str) -> str:
-    model = whisper.load_model("base")
+    model = whisper.load_model("small")
     result = model.transcribe(audio_path)
     return result['text'] 
