d68f42df929cfe70aab0060020f9b0e0f74fe6c7
Ahmet Serhat Kirici <ahmetserhat.kirici@gmail.com>
2025-05-20 13:01:01 +0300

    React app built

--- react-chat/src/api.js
+++ react-chat/src/api.js
@@ -1,6 +1,12 @@
 // Send a message to the FastAPI backend
+const getApiUrl = () => {
+  // Use the current hostname, but always port 8000
+  const host = window.location.hostname;
+  return `http://${host}:8000/message`;
+};
+
 export async function sendMessageToBackend({ message, image, audio_data, region, session_id }) {
-  const res = await fetch('http://localhost:8000/message', {
+  const res = await fetch(getApiUrl(), {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
